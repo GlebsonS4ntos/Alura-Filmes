5e50bc77a83f4890bba90eec8325360c3083c71d
Glebson Santos <glebsonsantos@outlook.pt>
2022-09-16 11:44:12 -0300

    geracao do token

---
+++ Alura/UsersApi/Services/TokenService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using UsersApi.Models;
+
+namespace UsersApi.Services
+{
+    public class TokenService
+    {
+        public Token CreateToken(IdentityUser<int> user)
+        {
+            Claim[] direitosUsuario = new[]
+            {
+                new Claim("username", user.UserName),
+                new Claim("id", user.Id.ToString())
+            };
+
+            var chave = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes("minha-chave-de-criptografia"));
+            var credenciais = new SigningCredentials(chave, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                claims: direitosUsuario,
+                signingCredentials: credenciais,
+                expires: DateTime.UtcNow.AddDays(1)
+                );
+        }
+    }
+}
